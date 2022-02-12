87cfdd0f3e5192bf79a6cdc361962e80cd693ba8
Dominykas Senkus <dominykas97@gmail.com>
2022-02-12 21:28:11 +0000

    refactoring

--- app/Board.py
+++ app/Board.py
@@ -1,29 +1,28 @@
 class Board():
-    id = 1
     lines = []
     direction_map = {
-        0:"N",
-        1:"E",
-        2:"S",
-        3:"W",
+        0: "N",
+        1: "E",
+        2: "S",
+        3: "W",
     }
     direction_map2 = {
-        "N":0,
-        "E":1,
-        "S":2,
-        "W":3,
+        "N": 0,
+        "E": 1,
+        "S": 2,
+        "W": 3,
     }
     scents = {}
+
     def __init__(self, filename):
 
-        f = open(filename,"r")
+        f = open(filename, "r")
         lines = f.read().splitlines()
         first_line = lines[0].split(" ")
         self.boardX = int(first_line[0])
         self.boardY = int(first_line[1])
         lines = lines[1:]
         print(first_line)
-        # print(lines[2].encode('raw_unicode_escape'))
         self.lines = lines
         for line in lines:
             print(line)
@@ -31,14 +30,16 @@ class Board():
 
     def get_x(self):
         return self.x
-    
+
     def get_y(self):
         return self.y
 
     def get_direction(self):
         return self.direction_map.get(self.direction)
 
-    
+    def get_status(self):
+        return f"{self.x} {self.y} {self.get_direction()}"
+
     def next(self):
         first_line = self.lines[0].split(" ")
         self.x = int(first_line[0])
@@ -46,61 +47,40 @@ class Board():
         self.direction = self.direction_map2.get(first_line[2])
         movement = self.lines[1]
         self.lines = self.lines[3:]
-        print(f"INIT: {self.x} {self.y} {self.get_direction()}")
+        print(f"INIT: {self.get_status()}")
 
-#  0 
-# 3 1 
-#  2
+        #  0
+        # 3 1
+        #  2
         for move in movement:
             if move == "L":
-                if self.direction > 0:
-                    self.direction -= 1
-                else:
-                    self.direction = 3
+                self.direction = (self.direction + 4 - 1) % 4
             if move == "R":
-                if self.direction < 3:
-                    self.direction += 1
-                else:
-                    self.direction = 0
-            print(self.direction)
+                self.direction = (self.direction + 1) % 4
+            scent = -1
             if move == "F":
-                if self.direction == 0:
-                    if self.y < self.boardY:
-                        self.y += 1
-                    else:
-                        if self.scents.get(f"{self.x},{self.y}") == 1:
-                            pass
-                        else:
-                            self.scents[f"{self.x},{self.y}"] = 1
-                            return f"{self.x} {self.y} {self.get_direction()} LOST"
-                if self.direction == 2:
-                    if self.y > 0:
-                        self.y -= 1
-                    else:
-                        if self.scents.get(f"{self.x},{self.y}") == 1:
-                            pass
-                        else:
-                            self.scents[f"{self.x},{self.y}"] = 1
-                            return f"{self.x} {self.y} {self.get_direction()} LOST"
-                if self.direction == 1:
-                    if self.x < self.boardX:
-                        self.x += 1
-                    else:
-                        if self.scents.get(f"{self.x},{self.y}") == 1:
-                            pass
-                        else:
-                            self.scents[f"{self.x},{self.y}"] = 1
-                            return f"{self.x} {self.y} {self.get_direction()} LOST"
-                if self.direction == 3:
-                    if self.x > 0:
-                        self.x -= 1
-                    else:
-                        if self.scents.get(f"{self.x},{self.y}") == 1:
-                            pass
-                        else:
-                            self.scents[f"{self.x},{self.y}"] = 1
-                            return f"{self.x} {self.y} {self.get_direction()} LOST"
+                if self.direction == 0 and self.y < self.boardY:
+                    self.y += 1
+                elif self.direction == 2 and self.y > 0:
+                    self.y -= 1
+                elif self.direction == 1 and self.x < self.boardX:
+                    self.x += 1
+                elif self.direction == 3 and self.x > 0:
+                    self.x -= 1
+                else:
+                    scent = self.check_present_scent()
 
+            print(self.direction)
             print(move)
             print(f"{self.x} {self.y} {self.get_direction()}")
-        return f"{self.x} {self.y} {self.get_direction()}"
+
+            if not scent:
+                return f"{self.get_status()} LOST"
+        return self.get_status()
+
+    def check_present_scent(self):
+        if f"{self.x},{self.y}" in self.scents:
+            return True
+        else:
+            self.scents[f"{self.x},{self.y}"] = 1
+            return False
