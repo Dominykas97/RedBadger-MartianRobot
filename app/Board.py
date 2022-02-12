6373dd6855bd7228314462cc5c6cf462626279be
Dominykas Senkus <dominykas97@gmail.com>
2022-02-12 20:15:22 +0000

    Movement implemented

--- app/Board.py
+++ app/Board.py
@@ -1,13 +1,64 @@
 class Board():
     id = 1
+    lines = []
+    direction_map = {
+        0:"N",
+        1:"E",
+        2:"S",
+        3:"W",
+    }
+    direction_map2 = {
+        "N":0,
+        "E":1,
+        "S":2,
+        "W":3,
+    }
     def __init__(self, filename):
 
         f = open(filename,"r")
         lines = f.read().splitlines()
         first_line = lines[0].split(" ")
-        self.x = int(first_line[0])
-        self.y = int(first_line[1])
+        self.boardX = int(first_line[0])
+        self.boardY = int(first_line[1])
         lines = lines[1:]
         print(first_line)
+        # print(lines[2].encode('raw_unicode_escape'))
+        self.lines = lines
         for line in lines:
             print(line)
+        f.close()
+    def get_direction(self):
+        return self.direction_map.get(self.direction)
+    def next(self):
+        first_line = self.lines[0].split(" ")
+        self.x = int(first_line[0])
+        self.y = int(first_line[1])
+        self.direction = self.direction_map2.get(first_line[2])
+        movement = self.lines[1]
+        self.lines[2:]
+
+#  0 
+# 3 1 
+#  2
+        for move in movement:
+            if move == "L":
+                if self.direction > 0:
+                    self.direction -= 1
+                else:
+                    self.direction = 3
+            if move == "R":
+                if self.direction < 3:
+                    self.direction += 1
+                else:
+                    self.direction = 0
+            print(self.direction)
+            if move == "F":
+                if self.direction == 0:
+                    self.y += 1
+                if self.direction == 2:
+                    self.y -= 1
+                if self.direction == 1:
+                    self.x += 1
+                if self.direction == 3:
+                    self.x -= 1
+            print(move)
--- tests/test_board.py
+++ tests/test_board.py
@@ -7,8 +7,14 @@ class TestBoard(unittest.TestCase):
 
 
     def test_board_init(self):
-        self.assertEqual(self.board.x, 5, "X should be 5")
-        self.assertEqual(self.board.y, 3, "Y should be 3")
+        self.assertEqual(self.board.boardX, 5, "boardX should be 5")
+        self.assertEqual(self.board.boardY, 3, "boardY should be 3")
+
+    def test_first_robot(self):
+        self.board.next()
+        self.assertEqual(self.board.x, 1, "X should be 1")
+        self.assertEqual(self.board.y, 1, "Y should be 1")
+        self.assertEqual(self.board.get_direction(), "E", "Direction should be E")
 
 
 if __name__ == '__main__':
